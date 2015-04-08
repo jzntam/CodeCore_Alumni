8718fc30a219d0cff17b2abf74d7f3f365adfada
Jason <cameljeet@gmail.com>
2015-04-08 14:38:00 -0700

    Changed Layout of index page to show data in a table -Also added uniqueness validation to title and default scope

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,5 @@
 class Cohort < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :details, presence: true
+  default_scope {order(:title => 'asc')}
 end
