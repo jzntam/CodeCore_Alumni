f4ed1c186d173c13db29a32b57b21af87d2370e5
Yushadha Bogoda <yushadha@gmail.com>
2015-04-09 16:45:53 -0700

    generate welcome comtroller and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :contacts
   end
 
-  root 'cohorts#index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
