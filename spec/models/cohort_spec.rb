d3bc6f3ea304298c6cee297b7a91b37f5ed9d264
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-04-10 14:31:27 -0700

    finished cohort model testing

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -3,12 +3,29 @@ require 'rails_helper'
 RSpec.describe Cohort, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe "Validations" do
-    def validation_attributes(new_attributes)
+    def validation_attributes(new_attributes ={})
       valid_attributes = {
         title: "valid title",
         details: "valid details"
       }
       valid_attributes.merge(new_attributes)
     end
+
+    it "requires a title" do
+      cohort = Cohort.new (validation_attributes({title: nil}))
+      expect(cohort).to be_invalid
+    end
+
+    it "requires details" do
+      cohort = Cohort.new (validation_attributes({details: nil}))
+      expect(cohort).to be_invalid
+    end
+
+    it "requires titles to be unique" do
+      cohort = Cohort.create(validation_attributes)
+      cohort_1 = Cohort.new(validation_attributes)
+      expect(cohort_1).to be_invalid
+    end
+
   end
 end
