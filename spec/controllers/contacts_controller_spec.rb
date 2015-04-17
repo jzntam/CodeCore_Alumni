c100a0ff9b9fe601931375d7f1773bd12e980e7c
Jason Tam <cameljeet@gmail.com>
2015-04-16 18:59:11 -0700

    Merge pull request #11 from jzntam/update_tests
    Fixed tests and remove unused specs

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,53 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
+  let(:cohort) { create(:cohort) }
+  let(:contact) { create(:contact) }
+  let(:user) { create(:user) }
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
+    before {login(user)}
     it "returns http success" do
-      get :show
+      get :show, cohort_id: cohort.id, id: contact.id
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #new" do
+    before {login(user)}
     it "returns http success" do
-      get :new
+      get :new, cohort_id: cohort.id
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    before {login(user)}
+    it "returns http found" do
+      post :create, cohort_id: cohort.id, contact: {first_name: "John"}
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(cohort_path(cohort)) 
     end
   end
 
   describe "GET #edit" do
+    before {login(user)}
     it "returns http success" do
-      get :edit
+      get :edit, cohort_id: cohort.id, id: contact.id
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe "PATCH #update" do
+    before {login(user)}
+    it "returns http found" do
+      cohort.contacts << contact
+      patch :update, id: contact.id, cohort_id: cohort.id, contact: { first_name: "John"}
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(cohort_path(cohort)) 
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "delete #destroy" do
+    before {login(user)}
+    it "returns http found" do
+      cohort.contacts << contact
+      delete :destroy, id: contact.id, cohort_id: cohort.id, contact: { first_name: "John"}
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(cohort_path(cohort)) 
     end
   end
 
