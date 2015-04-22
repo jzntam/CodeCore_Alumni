fd7ebab2c3468643299954a46a730a2bf2a9bfa6
Jason <cameljeet@gmail.com>
2015-04-22 10:44:27 -0700

    Generated Migration for additional contact columns -Also fixed the link text decoration issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420221220) do
+ActiveRecord::Schema.define(version: 20150422174219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20150420221220) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "position"
+    t.string   "project"
+    t.string   "github"
+    t.string   "linkedin"
   end
 
   add_index "contacts", ["cohort_id"], name: "index_contacts_on_cohort_id", using: :btree
