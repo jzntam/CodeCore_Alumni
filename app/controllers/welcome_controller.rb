39248974cd8d8b1f8e055e7366aa5c748d241bba
Jason <cameljeet@gmail.com>
2015-05-08 16:15:44 -0700

    Logged in user redirects to cohorts path, rather than splash page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   layout "landing"
+  before_action :logged_in?
+
   def index
   end
+
+  private
+
+  def logged_in?
+    if current_user.present?
+      redirect_to cohorts_path
+    end
+  end
+
 end
